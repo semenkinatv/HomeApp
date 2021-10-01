6958aa82e999f971bca9a2f85f911221da6a5677
SemenkinaTV <SemenkinaTV@arkona36.ru>
2021-10-01 03:27:02 +0300

    Страница с Погодой

--- HomeApp/HomeApp/Pages/MergeGridPage.xaml.cs
+++ HomeApp/HomeApp/Pages/MergeGridPage.xaml.cs
@@ -12,25 +12,12 @@ namespace HomeApp.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MergeGridPage : ContentPage
     {
+        private Grid grid;
         public MergeGridPage()
         {
             InitializeComponent();
-            //ScanOutside();
-            Grid grid = new Grid
-            {
-                // Набор строк 
-                RowDefinitions =
-              {
-                  new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
-                  new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
-                  new RowDefinition { Height = new GridLength(1, GridUnitType.Star) }
-               }
-            };
-            AddBlock(grid, Color.LightBlue, "InSide", "+ 25 °C", 0, 0);
-            AddBlock(grid, Color.LightBlue, "OutSide", "- 15 °C", 0, 1);
-            AddBlock(grid, Color.LightBlue, "Time", "12:12", 0, 2);
+ 
 
-            Content = grid;
         }
         public void AddBlock(Grid grid, Color color,string textName, string textTemp, int x, int y)
         {
@@ -47,7 +34,7 @@ namespace HomeApp.Pages
                    VerticalTextAlignment = TextAlignment.Start,
                    HorizontalTextAlignment = TextAlignment.Center,
                    FontSize = 36,
-                   TextColor = Color.Yellow
+                   TextColor = Color.Black
                },
                x, y);
             grid.Children.Add(
@@ -57,12 +44,32 @@ namespace HomeApp.Pages
                    VerticalTextAlignment = TextAlignment.Center,
                    HorizontalTextAlignment = TextAlignment.Center,
                    FontSize = 60,
-                   TextColor = Color.Yellow
+                   TextColor = Color.Black
                }, x, y);
 
            
         }
+        private void Weather_Click(object sender, EventArgs e)
+        {
+            grid = new Grid
+            {
+                // Набор строк 
+                RowDefinitions =
+              {
+                  new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                  new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                  new RowDefinition { Height = new GridLength(1, GridUnitType.Star) }
+               }
+            };
 
-    
+            Content = grid;
+            grid.BackgroundColor = Color.Black;
+
+            AddBlock(grid, Color.LightYellow, "Внутри", "+ 26 °C", 0, 0);
+            AddBlock(grid, Color.LightSkyBlue, "Снаружи", "- 15 °C", 0, 1);
+            AddBlock(grid, Color.LightSeaGreen, "Давление", "760 мм", 0, 2);
+
+        }
+
     }
 }
